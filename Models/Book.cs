de386fd4407b48bb7e97c06baf4ad62f99f6ef89
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-12-10 10:15:49 +0530

    book model restructured

--- Models/Book.cs
+++ Models/Book.cs
@@ -4,16 +4,16 @@ namespace BookApp.Models
 {
     public class Book
     {
-        [Key]
-        public int Id { get; set; }
+        [Key] 
+        public int Id { get; set; } 
 
-        [MaxLength(100)]
+        [MaxLength(100)] 
         public required string Title { get; set; }
 
         [MaxLength(100)]
         public required string Author { get; set; }
 
         [Required]
-        public DateTime Date_published { get; set; }
+        public DateTime Date_Published { get; set; }
     }
 }
