35667a56664ce771ddc53b445e89e226e020ff24
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-12-10 10:09:09 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BookApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public required string Title { get; set; }
+
+        [MaxLength(100)]
+        public required string Author { get; set; }
+
+        [Required]
+        public DateTime Date_published { get; set; }
+    }
+}
